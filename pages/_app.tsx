fac3b6438ecf123fd00c443623abe6b8082dd488
Bryan <bguenther3@gmail.com>
2021-08-11 21:29:10 -0700

    include color mode script and initialize in lightmode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import theme from '../theme/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Component {...pageProps} />
     </ChakraProvider>
   )
